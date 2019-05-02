7a7495ae2c4aeb7fe5292068e00c8355a2ce931c
Doug LaRue <doug.larue@gmail.com>
2019-05-02 14:12:33 -0700

    Initial update with new v2.1 features of running with a USB webcam and running headless.

--- carspeed.py
+++ carspeed.py
@@ -1,12 +1,159 @@
 # CarSpeed Version 2.0
+"""
+Script to capture moving car speed
+
+Usage:
+    carspeed.py [headless] [--config=<model> --use-webcam]
+
+Options:
+    -h --help     Show this screen.
+"""
+
+"""
+Updates:
+by Doug LaRue
+	original project location: https://gregtinkers.wordpress.com/2016/03/25/car-speed-detector/
+	Added --use-webcam option which adds USB WebCam capabilities
+	Added headless commandline option so when along with the --config option the software can run headless as a systemd service
+	Added --config monitoring_area.csv option
+	Added automatic saving of the monitorying area to CSV file
+"""
 
 # import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import time
 import math
 import datetime
 import cv2
+from docopt import docopt
+from pathlib import Path
+
+
+class CarSpeed():
+    def __init__(self, camera=None,  resolution=[640, 480],  freq=75, zero=1000):
+        self.camera = camera
+        self.resolution = resolution
+        self.freq = freq
+        if self.camera != None:
+            self.camera.set_mode()
+        self.config_file = None
+        self.headless = False
+        self.use_webcam = False
+        
+    def initialize(self):
+        print("We are in CarSpeed Initialize function!")
+        # get commandline options
+        args = docopt(__doc__)
+        print(args)
+        #cfg = dk.load_config()
+        if args['headless']:
+            self.headless=True
+        if args['--config']:
+            config = args['--config']
+            print('WITH CONFIG: self.config_file='+ config)
+            self.config_file = Path(config)
+            if self.config_file.is_file():
+                self.read_csv()
+            else:
+                print("config file does NOT exist")
+                self.config_file=None
+        if args['--use-webcam']:
+            self.use_webcam=True
+ 
+    def setup_webcam(self):
+        #global use_webcam
+        if self.use_webcam:
+            #import pygame
+            import pygame
+            import pygame.camera
+            self.cam = 0
+            self.resolution = (image_w, image_h)
+            pygame.init()
+            pygame.camera.init()
+            camList = pygame.camera.list_cameras()
+            print('cameras', camList)
+            self.cam = pygame.camera.Camera(camList[self.cam], self.resolution, "RGB")
+            self.cam.start()
+            #framerate = framerate
+            print('WebcamVideoStream loaded.. .warming camera')
+            time.sleep(2)
+        else:
+            # initialize the camera. Adjust vflip and hflip to reflect your camera's orientation
+            camera = PiCamera()
+            camera.resolution = RESOLUTION
+            camera.framerate = FPS
+            camera.vflip = False
+            camera.hflip = False
+            rawCapture = PiRGBArray(camera, size=camera.resolution)
+            # allow the camera to warm up
+            time.sleep(0.9)
+            
+    def set_resolution(self, horiz,  vert):
+        self.resolution=[horiz, vert]
+        self.camera.resolution(self.resolution)
+
+    def set_track_area(self, ix,  iy, fx,  fy):
+        if fx > ix:
+            self.upper_left_x = ix
+            self.lower_right_x = fx
+        else:
+            self.upper_left_x = fx
+            self.lower_right_x = ix
+
+        if fy > iy:
+            self.upper_left_y = iy
+            self.lower_right_y = fy
+        else:
+            self.upper_left_y = fy
+            self.lower_right_y = iy
+        self.monitored_width = self.lower_right_x - self.upper_left_x
+        self.monitored_height = self.lower_right_y - self.upper_left_y
+        #TODO
+        #save tracking area to file
+
+    def get_track_area(self):
+        #TODO
+        #load tracking area from file or get it from the GUI
+        self.set_track_area(ix,  iy, fx,  fy)
+  
+    def save_csv(self):
+        if SAVE_CSV:
+            csvfileout = "carspeed_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+            record_speed('Date,Day,Time,Speed,Image')
+            csvmonfileout = "monitoringarea{}.csv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+            record_mon_area('UpperLeftX,UpperLeftY,LowerRightX,LowerRightY,MonitoredWidth,MonitoredHeight')
+
+        if self.SAVE_CSV:
+            csvmonfileout = "monitoringarea{}.csv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+            record_mon_area(("%d" % upper_left_x)+','+("%d" % upper_left_y)+','+("%d" % lower_right_x)+','+("%d" % lower_right_y)+','+\
+                ("%d" % monitored_width)+','+("%d" % monitored_height))
+
+    def read_csv(self):
+        import csv
+        #self.config_file = config_file
+        with open(self.config_file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    #print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                else:
+                    #set_track_area(self, ix,  iy, fx,  fy)
+                    #print(f'\t iX {row[0]} iY {row[1]} fX {row[2]} fY {row[3]} MonitoredWidth {row[4]} MonitoredHeight {row[5]}.')
+                    self.set_track_area(int(row[0]),  int(row[1]), int(row[2]),  int(row[3]))
+                    line_count += 1
+            #print(f'Processed {line_count} lines.')
+    
+    def __del__(self):
+        self.camera.stop()
+
+#<--- END OF CarSpeed class -->
+
+
+#print("new Carspeed")
+#car = CarSpeed()
+#car.initialize()
+#   #car.setup_webcam()
 
 # place a prompt on the displayed image
 def prompt_on_image(txt):
@@ -33,6 +180,13 @@ def record_speed(res):
     f.write(res+"\n")
     f.close
 
+# record monitoring area in .csv format
+def record_mon_area(res):
+    global csvmonfileout
+    f = open(csvmonfileout, 'a')
+    f.write(res+"\n")
+    f.close
+
 # mouse callback function for drawing capture area
 def draw_rectangle(event,x,y,flags,param):
     global ix,iy,fx,fy,drawing,setup_complete,image, org_image, prompt
@@ -55,9 +209,9 @@ def draw_rectangle(event,x,y,flags,param):
         cv2.rectangle(image,(ix,iy),(fx,fy),(0,255,0),2)
         
 # define some constants
-DISTANCE = 76  #<---- enter your distance-to-road value here
+DISTANCE = 55 #<---- 54' 4.5" from center of road to outside wall.  #<---- enter your distance-to-road value here
 MIN_SPEED = 0  #<---- enter the minimum speed for saving images
-SAVE_CSV = False  #<---- record the results in .csv format in carspeed_(date).csv
+SAVE_CSV = True # False  #<---- record the results in .csv format in carspeed_(date).csv
 
 THRESHOLD = 15
 MIN_AREA = 175
@@ -115,38 +269,178 @@ tracking = False
 text_on_image = 'No cars'
 prompt = ''
 
+headless = False
+config_file = None
+use_webcam = False
+
+def read_csv():
+        global config_file
+        import csv
+        with open(str(config_file)) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    #print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                else:
+                    #set_track_area(self, ix,  iy, fx,  fy)
+                    #print(f'\t iX {row[0]} iY {row[1]} fX {row[2]} fY {row[3]} MonitoredWidth {row[4]} MonitoredHeight {row[5]}.')
+                    set_track_area(int(row[0]),  int(row[1]), int(row[2]),  int(row[3]))
+                    line_count += 1
+            #print(f'Processed {line_count} lines.')
+
+def initialize():
+    global headless,  config_file,  SHOW_IMAGE,  use_webcam
+    print("We are in Initialize function!")
+    # get commandline options
+    args = docopt(__doc__)
+    print(args)
+    if args['headless']:
+        headless=True
+        SHOW_IMAGE=False
+    if args['--config']:
+        config = args['--config']
+        print('WITH CONFIG: config_file='+ config)
+        config_file = Path(config)
+        if config_file.is_file():
+            read_csv()
+        else:
+            print("config file does NOT exist")
+            config_file=None
+    if args['--use-webcam']:
+        use_webcam=True
+
+def set_track_area(ixx,  iyy, fxx,  fyy):
+    global ix,  iy,  fx, fy, upper_left_x,  lower_right_x,  upper_left_y,  lower_right_y, monitored_width,  monitored_height
+    ix=ixx
+    iy=iyy
+    fx=fxx
+    fy=fyy
+    if fx > ix:
+        upper_left_x = ix
+        lower_right_x = fx
+    else:
+        upper_left_x = fx
+        lower_right_x = ix
+
+    if fy > iy:
+        upper_left_y = iy
+        lower_right_y = fy
+    else:
+        upper_left_y = fy
+        lower_right_y = iy
+    monitored_width = lower_right_x - upper_left_x
+    monitored_height = lower_right_y - upper_left_y
+
+image_w=640
+image_h=480
+image_d=3
+framerate = 20
+iCam = 0
+cam = 0
+
+#import pygame
+#import pygame.camera
+#rPi 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
 # initialize the camera. Adjust vflip and hflip to reflect your camera's orientation
-camera = PiCamera()
-camera.resolution = RESOLUTION
-camera.framerate = FPS
-camera.vflip = True
-camera.hflip = True
+def setup_camera():
+        global use_webcam,  cam,  rawCapture,  RESOLUTION,  FPS,  snapshot,  snapshot1
+        if use_webcam:
+            #global pygame
+            cam = 0
+            pygame.init()
+            pygame.camera.init()
+            camList = pygame.camera.list_cameras()
+            print('cameras', camList)
+            cam = pygame.camera.Camera(camList[cam], RESOLUTION, "RGB")
+            cam.start()
+            #framerate = framerate
+            print('WebcamVideoStream loaded.. .warming camera')
+            time.sleep(2)
+        else:
+            global picamera
+            # initialize the camera. Adjust vflip and hflip to reflect your camera's orientation
+            cam = PiCamera()
+            cam.resolution = RESOLUTION
+            cam.framerate = FPS
+            cam.vflip = False
+            cam.hflip = False
+            rawCapture = PiRGBArray(cam, size=cam.resolution)
+            # allow the camera to warm up
+            time.sleep(0.9)
 
-rawCapture = PiRGBArray(camera, size=camera.resolution)
-# allow the camera to warm up
-time.sleep(0.9)
+# setup headless, config etc on startup
+initialize()
+setup_camera()
 
+#import pygame.image
+#djl added if web-cam
+if use_webcam:
+  if cam.query_image():
+  #    global snapshot
+  #    global snapshot1
+  #    global frame
+    # snapshot = self.cam.get_image()
+    # self.frame = list(pygame.image.tostring(snapshot, "RGB", False))
+    snapshot = cam.get_image()
+    snapshot1 = pygame.transform.scale(snapshot, RESOLUTION)
+    frame = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(snapshot1, False, False), 90))
+
+#
+# HERE IS SOME GUI STUFF
+#
 # create an image window and place it in the upper left corner of the screen
-cv2.namedWindow("Speed Camera")
-cv2.moveWindow("Speed Camera", 10, 40)
+#if SHOW_IMAGE == True:
+if headless == False:
+    cv2.namedWindow("Speed Camera")
+    cv2.moveWindow("Speed Camera", 10, 40)
 
-# call the draw_rectangle routines when the mouse is used
-cv2.setMouseCallback('Speed Camera',draw_rectangle)
+    # call the draw_rectangle routines when the mouse is used
+    cv2.setMouseCallback('Speed Camera',draw_rectangle)
  
-# grab a reference image to use for drawing the monitored area's boundry
-camera.capture(rawCapture, format="bgr", use_video_port=True)
-image = rawCapture.array
-rawCapture.truncate(0)
-org_image = image.copy()
+camera = cam  #djl TODO: fix and go back to using "camera"
+image = None #djl 
+org_image = None #djl 
+#djl commented out rawCapture = snapshot
+#rawCapture = snapshot
+if use_webcam:
+    # grab a reference image to use for drawing the monitored area's boundry
+    img = cam.get_image()
+    img1 = pygame.transform.scale(img, RESOLUTION)
+    image = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(img1, False, False), 90))
+    # for pygame, the copy function makes the image numpy compatible
+    image = image.copy()
+    org_image = image.copy()
+else:
+    cam.capture(rawCapture, format="bgr", use_video_port=True)
+    image = rawCapture.array
+    rawCapture.truncate(0)
+    org_image = image.copy()
 
+
+
+# added section for saving of the monitoriing area
 if SAVE_CSV:
-    csvfileout = "carspeed_{}.cvs".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+    csvfileout = "carspeed_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
     record_speed('Date,Day,Time,Speed,Image')
 else:
     csvfileout = ''
 
+# preload config_file monitored area data if config_file defined, should be loaded by now
 prompt = "Define the monitored area - press 'c' to continue" 
-prompt_on_image(prompt)
+if config_file != None :
+    image = org_image.copy()
+    if headless == False:
+        prompt_on_image(prompt)
+        cv2.rectangle(image,(ix,iy),(fx,fy),(0,255,0),2)
+    setup_complete=True
+else:
+    if headless == False:
+        prompt_on_image(prompt)    
  
 # wait while the user draws the monitored area's boundry
 while not setup_complete:
@@ -181,26 +475,35 @@ else:
      
 monitored_width = lower_right_x - upper_left_x
 monitored_height = lower_right_y - upper_left_y
- 
-print("Monitored area:")
-print(" upper_left_x {}".format(upper_left_x))
-print(" upper_left_y {}".format(upper_left_y))
-print(" lower_right_x {}".format(lower_right_x))
-print(" lower_right_y {}".format(lower_right_y))
-print(" monitored_width {}".format(monitored_width))
-print(" monitored_height {}".format(monitored_height))
-print(" monitored_area {}".format(monitored_width * monitored_height))
- 
+
+if SAVE_CSV and config_file == None:
+    csvmonfileout = "monitoringarea_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+    record_mon_area('UpperLeftX,UpperLeftY,LowerRightX,LowerRightY,MonitoredWidth,MonitoredHeight')
+    record_mon_area(("%d" % upper_left_x)+','+("%d" % upper_left_y)+','+("%d" % lower_right_x)+','+("%d" % lower_right_y)+','+\
+        ("%d" % monitored_width)+','+("%d" % monitored_height))
+else:
+    csvmonfileout = '' 
+
 # capture frames from the camera (using capture_continuous.
 #   This keeps the picamera in capture mode - it doesn't need
 #   to prep for each frame's capture.
-
+#
+#rPi
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#while True: #for web_cam
+    if use_webcam:
+        img = cam.get_image()
+        img1 = pygame.transform.scale(img, RESOLUTION)
+
     #initialize the timestamp
     timestamp = datetime.datetime.now()
  
     # grab the raw NumPy array representing the image 
-    image = frame.array
+    if use_webcam:
+        image = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(img1, False, False), 90))
+        image = image.copy()
+    else:
+        image = frame.array
  
     # crop area defined by [y1:y2,x1:x2]
     gray = image[upper_left_y:lower_right_y,upper_left_x:lower_right_x]
@@ -213,8 +516,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if base_image is None:
         base_image = gray.copy().astype("float")
         lastTime = timestamp
-        rawCapture.truncate(0)
-        cv2.imshow("Speed Camera", image)
+        if not use_webcam:
+            rawCapture.truncate(0)
+        if headless == False:
+            cv2.imshow("Speed Camera", image)
   
     # compute the absolute difference between the current image and
     # base image and then turn eveything lighter gray than THRESHOLD into
@@ -266,7 +571,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 text_on_image = 'No Car Detected'
                 motion_found = False
                 biggest_area = 0
-                rawCapture.truncate(0)
+                #djl
+                if not use_webcam:
+                    rawCapture.truncate(0)
                 base_image = None
                 print('Resetting')
                 continue             
@@ -352,7 +659,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             break
          
     # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+    if not use_webcam:
+        rawCapture.truncate(0)
   
 # cleanup the camera and close any open windows
 cv2.destroyAllWindows()
